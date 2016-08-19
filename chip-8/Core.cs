4f93710285d9d04800fef03c5aa218c3b4cca798
Eugene Hamilton <genehamilton@gmail.com>
2016-08-19 11:53:56 -0500

    fixed SP decrement when popping stack in opcode:RET

--- chip-8/Core.cs
+++ chip-8/Core.cs
@@ -218,7 +218,7 @@ namespace chip_8
 
         private ushort popStack()
         {
-            return _s.stack[_s.SP--];
+            return _s.stack[--_s.SP];
         }
 
         private void LoadFonts()
